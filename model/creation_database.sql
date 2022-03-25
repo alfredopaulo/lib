0e9bb7b13fa29dc04fe9b1178f4f72b763b36f1c
Alfredo <alfredopaulo2003@gmail.com>
2022-03-25 20:43:46 -0300

    database e admin

---
+++ model/creation_database.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS `usuarios` (
+      `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+      `nome` VARCHAR( 50 ) NOT NULL ,
+      `usuario` VARCHAR( 25 ) NOT NULL ,
+      `senha` VARCHAR( 40 ) NOT NULL ,
+      `email` VARCHAR( 100 ) NOT NULL ,
+      `nivel` INT(1) UNSIGNED NOT NULL DEFAULT '1',
+      `ativo` BOOL NOT NULL DEFAULT '1',
+      `cadastro` DATETIME NOT NULL ,
+  	`logradouro` VARCHAR(100) NOT NULL, 
+	`numero` VARCHAR(100) NOT NULL,
+	`complemento` VARCHAR(100) NOT NULL,
+	`bairro` VARCHAR(100) NOT NULL,
+	`cep` VARCHAR(100) NOT NULL,
+	`cidade` VARCHAR(100) NOT NULL,
+	`uf` VARCHAR(10) NOT NULL,
+	`cpf` VARCHAR(100) NOT NULL,
+      PRIMARY KEY (`id`)
+)
+
+INSERT INTO `usuarios` VALUES (NULL, 'Usuário Teste', 'demo', SHA1( 'demo'), 'usuario@demo.com.br', 1, 1, NOW( ),'17','jauari','69280','manicore','AM','027345231','123','123');
