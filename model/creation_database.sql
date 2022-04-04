4e2b9a5f48865ab49f1c1d4a7b26d41258c11d29
Waleson Melo <walesonmelo23@gmail.com>
2022-04-03 23:31:17 -0400

    adicionada tabela aluno

--- model/creation_database.sql
+++ model/creation_database.sql
@@ -16,7 +16,23 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 	`uf` VARCHAR(10) NOT NULL,
 	`cpf` VARCHAR(100) NOT NULL,
       PRIMARY KEY (`id`)
-)
+);
+
+CREATE TABLE IF NOT EXISTS aluno(
+    id integer PRIMARY KEY AUTO_INCREMENT,
+    matricula varchar(10) NOT NULL,
+    cpf varchar(11) NOT NULL,
+    nome varchar(60) NOT NULL,
+    email varchar(60) NOT NULL,
+    logradouro varchar(60) NOT NULL,
+    numero varchar(5) NOT NULL,
+    bairro varchar(50) NOT NULL,
+    cidade varchar(50) NOT NULL,
+    estado varchar(50) NOT NULL,
+    cep varchar(10) NOT NULL,
+    complemento varchar(70),
+    status_aluno int NOT NULL DEFAULT '1'
+);
 
 CREATE TABLE IF NOT EXIST `livros`(
  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -24,7 +40,7 @@ CREATE TABLE IF NOT EXIST `livros`(
  `autor` VARCHAR( 40 ) NOT NULL ,
  `status` int(2) NOT NULL ,
   PRIMARY KEY (`id`)
-
-)
+);
 
 INSERT INTO `usuarios` VALUES (NULL, 'Usuário Teste', 'demo', SHA1( 'demo'), 'usuario@demo.com.br', 1, 1, NOW( ),'17','jauari','69280','manicore','AM','027345231','123','123');
+INSERT INTO `aluno` (`id`, `matricula`, `cpf`, `nome`, `email`, `logradouro`, `numero`, `bairro`, `cidade`, `estado`, `cep`, `complemento`, `status_aluno`) VALUES (NULL, '123123', '12312312312', 'teste', 'teste@teste.com', 'teste', '1', 'teste', 'teste', 'teste', '123123', 'testes', '1');
