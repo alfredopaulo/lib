4d61bff45e2545d7860d41dc82ec8df9b2b46245
Waleson Melo <walesonmelo23@gmail.com>
2022-04-13 19:14:46 -0400

    update alterarAluno.php

--- view/Aluno/formAlterarAluno.php
+++ view/Aluno/formAlterarAluno.php
@@ -1,5 +1,29 @@
 <!DOCTYPE html>
+<?php
 
+use JetBrains\PhpStorm\ArrayShape;
+
+  include('../../model/database.php');
+  include('../../model/Aluno.php');
+
+  $menssage;
+
+  $action = filter_input(INPUT_POST, 'actionn');
+
+  if (isset($_POST['actionn']) && $_POST['actionn'] == 'search') {
+    $matricula = $_POST['matriculap'];
+    $dados_aluno = pesquisarAluno($matricula);
+    // var_dump($dados_aluno);
+    // echo($dados_aluno[0]["nome"]);
+
+    if(!$dados_aluno){
+      $menssage = "Aluno não encontrado";
+    } else{
+      $menssage = "";
+    }
+  }
+
+?>
 <head>
 
   <link rel="stylesheet" href="/lib/view/styles/main.css">
@@ -24,133 +48,139 @@
       <a href="login.html">Sair</a>
     </div>
   </header>
-
+<h3 style="color: #ffffff;">
+  <?php 
+    echo($menssage);
+  ?>
+</h3>
   <section class="login-cadastro">
-
-    <form class="card-cadastro" action="../../index.php" method="post">
-
+    <div class="card-cadastro">
       <div class="card-header">
 
         <h2>Alterar Aluno</h2>
 
       </div>
 
-      <input type="hidden" name="action" value="alterarAluno">
+      <form action="./formAlterarAluno.php"  method="post">
+        <div class="search-content">
+            <div class="search-area">
 
-      <div class="search-content">
-        <div class="search-area">
+              <input type="text" id="matriculap" name="matriculap" value="" required placeholder="Pesquisar Matrícula">
+            
+            </div>
+            <div class="search-area">
 
-          <input type="text" id="matriculap" name="matriculap" value="" required placeholder="Pesquisar Matrícula">
-          <input type="hidden" name="id" id="id">
-        
-        </div>
-        <div class="search-area">
+                <input type="hidden" name="actionn" value="search">
+                <button id="search" type="submit"><i class="fa fa-search"></i></button>
+                      
+            </div>
+          
+            <style>
+              .search-content{
+                display: flex;
+                flex-direction: row;
+              }
+              .search-area input {
+                width: 349px;
+                padding: 15px;
+                box-sizing: border-box;
+                background-color: #272323;
+                border-radius: 10px;
+                border-style: none;
+                color: #ffffff;
+                font-size: 90%;
+                margin-bottom: 10px;
+                margin-right: 5px;
+              }
+              #search {
+                cursor: pointer;
+                padding-top: 15px;
+                padding-bottom: 15px;
+                padding-left: 30px;
+                padding-right: 30px;
+                width: 100%;
+                background-color: #b61d1d;
+                border-radius: 10px;
+                border-style: none;
+                color: #ffffff;
+                font-size: 90%;
+                border-style: none;
+                cursor: pointer;
+                font-weight: bold;
+              }
+            </style>
 
-          <button id="search" type="submit"><i class="fa fa-search"></i></button>
-        
-        </div>
-        <style>
-          .search-content{
-            display: flex;
-            flex-direction: row;
-          }
-          .search-area input {
-            width: 349px;
-            padding: 15px;
-            box-sizing: border-box;
-            background-color: #272323;
-            border-radius: 10px;
-            border-style: none;
-            color: #ffffff;
-            font-size: 90%;
-            margin-bottom: 10px;
-            margin-right: 5px;
-          }
-          #search {
-            cursor: pointer;
-            padding-top: 15px;
-            padding-bottom: 15px;
-            padding-left: 30px;
-            padding-right: 30px;
-            width: 100%;
-            background-color: #b61d1d;
-            border-radius: 10px;
-            border-style: none;
-            color: #ffffff;
-            font-size: 90%;
-            border-style: none;
-            cursor: pointer;
-            font-weight: bold;
-          }
-        </style>
+          </div>
+        </form>  
 
-      </div>
+      <form  action="../../index.php" method="post">
 
-      <div class="card-content">
-        <div class="card-cadastro-area">
+        <input type="hidden" name="action" value="alterarAluno">
+        <input type="hidden" name="id" id="id" value="<?php echo($dados_aluno[0]["id"]); ?>"> 
 
-          <label for="nome">Nome</label>
-          <input type="text" id="nome" name="nome" value="">
+        <div class="card-content">
+          <div class="card-cadastro-area">
+            <label for="nome">Nome</label>
+            <input type="text" id="nome" name="nome" value="<?php echo($dados_aluno[0]["nome"]); ?>">
+          </div>
 
-        </div>
+          <div class="card-cadastro-area">
 
-        <div class="card-cadastro-area">
+            <label for="matricula">Matrícula</label>
+            <input type="text" id="matricula" name="matricula" value="<?php echo($dados_aluno[0]["matricula"]); ?>">
 
-          <label for="matricula">Matrícula</label>
-          <input type="text" id="matricula" name="matricula" value="">
-
-        </div>
-
-      </div>
-
-      <div class="card-content">
-
-        <div class="card-cadastro-area">
-
-          <label for="email">Email</label>
-          <input type="email" id="email" name="email" value="">
+          </div>
 
         </div>
 
-        <div class="card-cadastro-area">
+        <div class="card-content">
 
-          <label for="cpf">CPF</label>
+          <div class="card-cadastro-area">
 
-          <input type="text" id="cpf" name="cpf" value="">
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" value="<?php echo($dados_aluno[0]["email"]); ?>">
 
-        </div>
+          </div>
 
-      </div>
+          <div class="card-cadastro-area">
 
-      <div class="card-content">
+            <label for="cpf">CPF</label>
 
-        <div class="card-cadastro-area">
+            <input type="text" id="cpf" name="cpf" value="<?php echo($dados_aluno[0]["cpf"]); ?>">
 
-          <label for="telefone">Telefone</label>
-          <input type="text" id="telefone" name="telefone" value="">
-
-        </div>
-
-        <div class="card-cadastro-area">
-
-          <label for="endereco">Endereço</label>
-          <input type="text" id="endereco" name="endereco" value="">
+          </div>
 
         </div>
 
-        <div class="card-footer">
+        <div class="card-content">
 
-          <input type="submit" value="Cadastrar" class="submit">
+          <div class="card-cadastro-area">
+
+            <label for="telefone">Telefone</label>
+            <input type="text" id="telefone" name="telefone" value="<?php echo($dados_aluno[0]["telefone"]); ?>">
+
+          </div>
+
+          <div class="card-cadastro-area">
+
+            <label for="endereco">Endereço</label>
+            <input type="text" id="endereco" name="endereco" value="<?php echo($dados_aluno[0]["endereco"]); ?>">
+
+          </div>
+
+          <div class="card-footer">
+
+            <input type="submit" value="Cadastrar" class="submit">
+
+          </div>
 
         </div>
+        <div class="excluir">
+          <a href="/lib/view/Aluno/excluirAluno.php">Excluir Aluno</a>
+        </div>
 
-      </div>
-      <div class="excluir">
-        <a href="/lib/view/Aluno/excluirAluno.php">Excluir Aluno</a>
-      </div>
-
-    </form>
-
+      </form>
+    </div>
   </section>
   <footer>
     <P>LAWHACK</P>
